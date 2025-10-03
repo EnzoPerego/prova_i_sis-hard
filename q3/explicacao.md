5308cb6f61b60d81c2c67748d93f4c119b207ca7
EnzoPerego <159027526+EnzoPerego@users.noreply.github.com>
2025-10-03 17:28:59 +0000

    Final

--- q3/explicacao.md
+++ q3/explicacao.md
@@ -6,3 +6,4 @@ Deixe aqui a sua explicação do que a função `funcao1` faz. Faça uma explica
 Ex: se você acha que é uma função que calcula o fatorial de um parâmetro, basta escrever aqui "Calcula e retorna o fatorial do parâmetro utilizando..."
 
 Sua resposta:
+A funcao calcula e retorna o indice de onde esta o valor que ele esta procurando (edx) dentro de uma memoria.
--- q3/solucao.c
+++ q3/solucao.c
@@ -1,11 +1,11 @@
 // Seu código da funcao1_solucao
-int funcao1_solucao(long rdi, int esi, int edx){
+int funcao1_solucao(int *rdi, int esi, int edx){
     int eax = 0;
-    if(eax >= esi){
-        return -1;
-    }
-    while(edx != (rdi + eax * 4)){
+    while(edx != rdi[eax]){
         eax += 1;
+        if(eax >= esi){
+            return -1;
+        }
     }
 
     return eax;
